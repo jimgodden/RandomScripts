99131105eb080f35c17f391f3c75949398216ac0
jimgodden <jgodden90@outlook.com>
2023-04-10 12:31:35 -0400

    updated comments

--- WIndows/OpenWindowsFirewallPorts.ps1
+++ WIndows/OpenWindowsFirewallPorts.ps1
@@ -1,8 +1,8 @@
 # Allow TCP port 3389 through Windows Firewall on Windows Server 2022
 New-NetFirewallRule -DisplayName "Allow RDP" -Direction Inbound -Protocol TCP -LocalPort 3389 -Action Allow
 
-# Allow TCP port 3389 through Windows Firewall on Windows Server 2022
+# Allow TCP port 443 through Windows Firewall on Windows Server 2022
 New-NetFirewallRule -DisplayName "Allow HTTPS" -Direction Inbound -Protocol TCP -LocalPort 443 -Action Allow
 
-# Allow TCP port 3389 through Windows Firewall on Windows Server 2022
+# Allow TCP port 80 through Windows Firewall on Windows Server 2022
 New-NetFirewallRule -DisplayName "Allow HTTP" -Direction Inbound -Protocol TCP -LocalPort 80 -Action Allow
